ace62896bd687c9d00de08923669259eead4d4dd
Yanick1123 <46910241+Yanick1123@users.noreply.github.com>
2022-12-05 13:45:31 -0500

    Update SLURM_sabre.sh

--- SLURM_sabre.sh
+++ SLURM_sabre.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
-#SBATCH -D /project/fbelzile/users/yaass9/homework/results/barcodes
+#SBATCH -D PATH/TO/WORKING/DIRECTORY
 #SBATCH -J sabre	
 #SBATCH -o sabre%j.out
 #SBATCH -c 1
-#SBATCH -p soyagen	
+#SBATCH -p PARTITION/NAME	
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=yanick.asselin.1@ulaval.ca
+#SBATCH --mail-user=EMAIL/ADDRESS
 #SBATCH --time=1-00:00
 #SBATCH --mem=1G 
 
 # Load the software with module if applicable:
 module load sabre/1.000
 
-DATA=/project/fbelzile/users/yaass9/homework/data/FC20150701_1.fq.gz
-BARCODE=/project/fbelzile/users/yaass9/homework/data/FC20150701_1.txt
-#TOOL=path/to/sabre
+DATA=PATH/TO/SEQ/DATA
+BARCODE=PATH/TO/BARCODE/FILE
+
 
 exec &> sabre.log
 
 sabre se -f $DATA -b $BARCODE -u unk.fastq
 
-#sabre se -f /project/fbelzile/users/yaass9/homework/data/FC20150701_1.fq.gz -b /project/fbelzile/users/yaass9/homework/data/FC20150701_1.txt -u unk.fastq
